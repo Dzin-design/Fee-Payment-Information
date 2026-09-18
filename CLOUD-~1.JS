16b236ed2bd491961e8692f20e8414f002873167
DashBoard <sopontle@gmail.com>
2026-09-18 13:37:42 +0700

    Delete CLOUD-~1.JS

--- CLOUD-~1.JS
+++
@@ -1,4 +0,0 @@
-// ตั้งค่าการเชื่อมต่อ Cloud สำหรับ Dashboard แบบไม่ต้อง Login
-// ใช้ Publishable key เท่านั้น
-window.FEE_SUPABASE_URL = "https://ybhjvabjdaflukmsmvvq.supabase.co";
-window.FEE_SUPABASE_PUBLISHABLE_KEY = "sb_publishable_B25BJjltQFqv2GEwWvdlGA_pxUUag8F";
